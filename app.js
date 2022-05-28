fd2bad8dcd4959b5b99ab9d6b787848adceba476
Norseng <esina.helena@gmail.com>
2022-05-28 13:24:06 +0300

    refactor: CSS&JS YAGNI approach implemented

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ let addButton = document.getElementById("add-task");
 let incompleteTaskHolder = document.getElementById("incomplete-tasks");
 let completedTasksHolder = document.getElementById("completed-tasks");
 
-// New task list item
 let createNewTaskElement = (taskValue) => {
-
     let listItem = document.createElement("li");
     listItem.classList.add("task-list__task");
     
@@ -39,23 +37,16 @@ let createNewTaskElement = (taskValue) => {
     listItem.appendChild(deleteButton);
     return listItem;
 }
-
 let addTask = () => {
-    console.log("Add Task...");
-    //Create a new list item with the text from the #new-task:
     if (!newTaskInput.value) return;
     let listItem = createNewTaskElement(newTaskInput.value);
 
-    //Append listItem to incompleteTaskHolder
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskCompleted);
 
     newTaskInput.value="";
 }
 let editTask = function() {
-    console.log("Edit Task...");
-    console.log("Change 'edit' to 'save'");
-
     let listItem = this.parentNode;
     let editBtn = listItem.querySelector(".edit-task-button");
     let containsClass = listItem.classList.contains("task-list__task_editable");
@@ -68,49 +59,34 @@ let editTask = function() {
     listItem.classList.toggle("task-list__task_editable");
 };
 let deleteTask = function() {
-    console.log("Delete Task...");
-
     let listItem = this.parentNode;
     let ul = listItem.parentNode;
     
     ul.removeChild(listItem);
 }
 let taskCompleted = function() {
-    console.log("Complete Task...");
-
     let listItem = this.parentNode;
     listItem.classList.add("task-list__task_completed");
     completedTasksHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskIncomplete);
 }
 let taskIncomplete = function() {
-    console.log("Incomplete Task...");
-
     let listItem = this.parentNode;
     listItem.classList.remove("task-list__task_completed");
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
 }
-let ajaxRequest = function() {
-    console.log("AJAX Request");
-}
 
 addButton.onclick = addTask;
 addButton.addEventListener("click", addTask);
-addButton.addEventListener("click", ajaxRequest);
 
 let bindTaskEvents = (taskListItem,checkBoxEventHandler) => {
-    console.log("bind list item events");
-    //select ListItems children
     let checkBox = taskListItem.querySelector(".task-list__input-checkbox");
     let editButton = taskListItem.querySelector(".edit-task-button");
     let deleteButton = taskListItem.querySelector(".delete-task-button");
-
-    //Bind editTask to edit button.
+    
     editButton.onclick = editTask;
-    //Bind deleteTask to delete button.
     deleteButton.onclick = deleteTask;
-    //Bind taskCompleted to checkBoxEventHandler.
     checkBox.onchange = checkBoxEventHandler;
 }
 for (let i=0; i< incompleteTaskHolder.children.length; i++){
